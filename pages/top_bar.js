8bc4f4a68eeceb903c2e92fc45284124b7f2e5ed
mktaylor3 <mktaylor3@bsu.edu>
2020-04-24 05:36:19 -0400

    Fix Menu Button Updated the navigation for v5

--- pages/top_bar.js
+++ pages/top_bar.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 import Styles from './Styles';
+import Icon from 'react-native-vector-icons/Ionicons'
+import { useNavigation } from '@react-navigation/native';
 
 
 export default class TopBar extends Component {
@@ -9,22 +11,23 @@ export default class TopBar extends Component {
     
   }
 
- /*
+ 
   DrawerIcon = () => {
+    const navigation = useNavigation();
     return(
     <View>
-        <TouchableOpacity onPress={() => {this.openDrawer(); } }>
+        <TouchableOpacity onPress={() => {navigation.toggleDrawer(); } }>
             <Icon name="ios-menu" style={{padding: 10}} size={28} color="black" type={"font-awesome"}/>
         </TouchableOpacity>
     </View>
     );
 };
-*/
+
 
   render() {
     return(
       <View style={{flexDirection:"row", flex:.6}}>
-        
+        <this.DrawerIcon/>
         <Text style={[Styles.title, {flex:.8}]}>
           MUNCIE EVENTS
         </Text>
